f0d8adef7145a7494b5138db0e0815ba741d771b
Sophistafunk <jchristo87@gmail.com>
2018-07-24 22:38:19 -0700

    changes

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,15 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+operands = form.getlist('operand')
+
+try:
+    total = sum(map(int, operands))
+    body = f'The total is: {total}'
+except (ValueError, TypeError):
+    body = 'Invalid numbers provided, please provide integer numbers'
+
 print('Content-type: text/plain')
 print('')
-print('Your job is to make this work')
+print(body)
